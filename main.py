b38f0d8a2783cc9db32f9afbca9db69bff83bda7
IssaHassan <Issa.Hassan@carleton.ca>
2017-10-14 23:12:02 -0400

    Create class match to hold functions print_keypoints and get_keypoints

--- main.py
+++ main.py
@@ -9,26 +9,34 @@ IMAGE_PATH = 'photos/img_6727.jpg'
 pattern = cv2.imread(PATTERN_PATH,0)
 img = cv2.imread(IMAGE_PATH,0)
 
-def get_keypoints(image):
+class Match:
 	
-	orb = cv2.ORB_create()
-	#returns list of keypoints form the image.
-	return orb.detect(image,None)
+	def __init__(self, filename):
+		
+		self.iphone_img = cv2.imread(filename)
+		self.orb = cv2.ORB_create()
 
-def print_keypoints(kp,image):
-	
-	"""
-		draw markers on iphone image to show keypoints
-	"""
-	image_kp = cv2.drawKeypoints(image,kp,255, 0)
-	plt.imshow(image_kp)
-	plt.show()
+	def get_keypoints(self,image):
+		
+		#returns list of keypoints form the image.
+		return self.orb.detect(image,None)
+
+	def print_keypoints(self,kp,image):
+		
+		"""
+			draw markers on iphone image to show keypoints
+		"""
+		image_kp = cv2.drawKeypoints(image,kp,255, 0)
+		plt.imshow(image_kp)
+		plt.show()
 
 def main():
-	print_keypoints(get_keypoints(pattern),pattern)
-	print_keypoints(get_keypoints(img),img)
 	
-
+	m = Match(IMAGE_PATH)
+	m.print_keypoints(m.get_keypoints(pattern),pattern)
+	m.print_keypoints(m.get_keypoints(m.iphone_img),m.iphone_img)
+	
+	
 if __name__ == "__main__":
 	main()
 	
